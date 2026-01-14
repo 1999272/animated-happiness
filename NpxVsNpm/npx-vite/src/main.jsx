198f7242a1b2a6c8b8de5ebcf8a593dbb0b32863
Arthur-codeX <daniel.karanja239@gmail.com>
2026-01-14 09:16:33 +0300

    updated code

---
+++ NpxVsNpm/npx-vite/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    Hello World i am react
+    <div>This is cool</div>
+    <h1>This is hi</h1>
+  </StrictMode>
+);
